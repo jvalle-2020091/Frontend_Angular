6830dbdb255a3159b52f10afc4db69cf3e216870
Jose Ruiz <jruiz-2019069@kinal.edu.gt>
2022-09-21 15:13:45 -0600

    Se modifico el componente de menu

--- Frontend_Angular/src/app/layout/app.menu.component.ts
+++ Frontend_Angular/src/app/layout/app.menu.component.ts
@@ -4,107 +4,92 @@ import { LayoutService } from './service/app.layout.service';
 import { RoleRestService } from '../services/role-rest.service';
 import { TranslateService } from '@ngx-translate/core';
 import { LoginRestService } from '../services/login-rest.service';
+import { permissions } from '../../../src/app/demo/components/pages/roles/permissions';
 
 @Component({
-    selector: 'app-menu',
-    templateUrl: './app.menu.component.html'
+  selector: 'app-menu',
+  templateUrl: './app.menu.component.html'
 })
 export class AppMenuComponent implements OnInit {
 
-    permissionsStrings: any = [];
+  permissionsStrings: any = [];
+  permissionsIdFunctions: any = [];
 
-     //GET PERMISSION
-     permissions(){
-        this.loginRest.permissions(this.loginRest.getUser().id).subscribe({
-          next: (res: any)=> {
-            this.permissionsStrings = res.nameFunctions;
-          },
-          error: (err) => {
-            console.log(err);
-          }
-        });
-      }
+  //GET PERMISSION
+  permissions() {
+    this.loginRest.permissions(this.loginRest.getUser().id).subscribe({
+      next: (res: any) => {
+        this.permissionsIdFunctions = res.idFunctions;
+        console.log(this.permissionsIdFunctions);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 
-    permissionGetUsers(){
-        let permissions = this.permissionsStrings;
-        let bandera: boolean = false;
-        if(permissions != undefined){
-          for(let x = 0; x < permissions.length; x++){
-              if(permissions[x] == 'Get users'){
-                bandera = true;
-                break;
-              }
-          }
-        }
-        return bandera;
-      }
-      false: boolean = false;
-      true: boolean = true;
+  setLang: any
 
-    model: any = [
-        // {
-        //     label: 'MENU.HOME',
-        //     items: [
-        //         { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/layout'] }
-        //     ]
-        // },
-       
-        {
-            label: 'MENU.COMPONENTS',
-            icon: 'pi pi-fw pi-briefcase',
-            routerLink: ['/pages'],
-            items: [
-              
-                {
-                    label: "MENU.USERS",
-                    icon: 'pi pi-fw pi-users',
-                    routerLink: ['/layout/pages/crud'],
-                    //hidden:this.false
-                },
-                {
-                    label: 'MENU.ROLES',
-                    icon: 'pi pi-fw pi-cog',
-                    routerLink: ['/layout/pages/roles'],
-                    //hidden:this.true
-                },
-               
-            ]
-        },
-    ];
+  constructor(
+    public layoutService: LayoutService,
+    public translate: TranslateService,
+    private roleRest: RoleRestService,
+    private loginRest: LoginRestService) {
 
-    setLang: any
+  }
 
-    constructor(
-        public layoutService: LayoutService,
-        public translate: TranslateService,
-        private roleRest: RoleRestService,
-        private loginRest: LoginRestService) 
-                {
-                    
-                }
+  ngOnInit() {
+    this.permissions();
 
-    ngOnInit() {
-        this.permissions();    
+    this.setLang = this.roleRest.getLanguage();
+    this.translate.addLangs(['es', 'en']);
+    this.translate.setDefaultLang(this.setLang)
 
-        this.setLang = this.roleRest.getLanguage();
-        this.translate.addLangs(['es', 'en']);
-        this.translate.setDefaultLang(this.setLang)
+    for (const item of this.model) {
+      this.translate.get(item.label).subscribe(res => {
+        item.label = res
+      })
+      item.items.map((child: any) => {
 
-        for(const item of this.model){
-            this.translate.get(item.label).subscribe(res =>{
-                item.label = res
-            })
-            item.items.map((child:any) =>{
-            
-                this.translate.get(child.label).subscribe(res =>{
-                    child.label = res
-                })
-                
-            })
-        }
+        this.translate.get(child.label).subscribe(res => {
+          child.label = res
+        })
+
+      })
     }
-   
-     
-      
+  }
 
+  model: any = [
+    // {
+    //     label: 'MENU.HOME',
+    //     items: [
+    //         { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/layout'] }
+    //     ]
+    // },
+    {
+      label: 'MENU.COMPONENTS',
+      icon: 'pi pi-fw pi-briefcase',
+      routerLink: ['/pages'],
+      items: [
+        {
+          label: 'MENU.USERS',
+          icon: 'pi pi-fw pi-users',
+          routerLink: ['/layout/pages/crud'],
+          visible: this.getPermission(permissions.Get_users),
+        },
+        {
+          label: 'MENU.ROLES',
+          icon: 'pi pi-fw pi-cog',
+          routerLink: ['/layout/pages/roles'],
+          //hidden:this.true
+        },
+      ],
+    },
+  ];
+
+  // Metodo para validar permisos de un usuario
+  public getPermission(id_Function: number): boolean {
+    console.log(this.permissionsIdFunctions.includes(id_Function))
+    return this.permissionsIdFunctions.includes(id_Function);
+  }
 }
